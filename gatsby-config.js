633cbca5ad0bf94b9849210230ed640758f872e0
Siddharth Mantri <siddharthmantri1@gmail.com>
2020-04-07 17:59:23 +0200

    Fixed gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Sid Mantri - Front End Developer`,
+    description: `Design and Technology have always been the heart of all my interests.`,
+    author: `Siddharth Mantri`,
   },
   plugins: [
     `gatsby-plugin-typescript`,
