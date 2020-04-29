574a13b432e1113d723c0c7f13c4315e6e6fbe36
Siddharth Mantri <siddharthmantri1@gmail.com>
2020-04-29 15:25:53 +0200

    Fixed overflow on fade in

--- src/components/Fade/index.tsx
+++ src/components/Fade/index.tsx
@@ -1,15 +1,20 @@
 import React from "react"
 import styled from "styled-components"
 
+
+const translate = 0;
+
+
 const Fade = styled.div<{ fade: number }>`
   -moz-animation: fadein ${props => props.fade}s;
   -webkit-animation: fadein ${props => props.fade}s;
   -o-animation: fadein ${props => props.fade}s;
   animation: fadein ${props => props.fade}s;
+  width: 100%;
   @keyframes fadein {
     from {
       opacity: 0;
-      transform: translateY(10px);
+      transform: translateY(${translate}px);
     }
     to {
       opacity: 1;
@@ -18,6 +23,7 @@ const Fade = styled.div<{ fade: number }>`
   @-moz-keyframes fadein {
     from {
       opacity: 0;
+      transform: translateY(${translate}px);
     }
     to {
       opacity: 1;
@@ -26,6 +32,7 @@ const Fade = styled.div<{ fade: number }>`
   @-webkit-keyframes fadein {
     from {
       opacity: 0;
+      transform: translateY(${translate}px);
     }
     to {
       opacity: 1;
@@ -34,6 +41,7 @@ const Fade = styled.div<{ fade: number }>`
   @-o-keyframes fadein {
     from {
       opacity: 0;
+      transform: translateY(${translate}px);
     }
     to {
       opacity: 1;
